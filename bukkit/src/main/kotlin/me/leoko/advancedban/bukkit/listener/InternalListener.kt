9f75cdd79bf6b35c742513697bf94f947a34643e
Dgau-glitch <dgau5@mail.ru>
2026-05-16 00:46:13 +0700

    Refine InternalListener Kotlin with DRY helpers for ban list access

--- bukkit/src/main/kotlin/me/leoko/advancedban/bukkit/listener/InternalListener.kt
+++ bukkit/src/main/kotlin/me/leoko/advancedban/bukkit/listener/InternalListener.kt
@@ -9,41 +9,36 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import java.util.Date
 
+@Suppress("DEPRECATION")
 class InternalListener : Listener {
     @Suppress("UNCHECKED_CAST")
+    private fun nameBanList(): BanList<Any> = Bukkit.getBanList(BanList.Type.NAME) as BanList<Any>
+
+    @Suppress("UNCHECKED_CAST")
+    private fun ipBanList(): BanList<Any> = Bukkit.getBanList(BanList.Type.IP) as BanList<Any>
+
     @EventHandler
     fun onPunish(event: PunishmentEvent) {
         val punishment = event.punishment
         when (punishment.type) {
             PunishmentType.BAN, PunishmentType.TEMP_BAN -> {
-                val banList = Bukkit.getBanList(BanList.Type.NAME) as BanList<Any>
-                banList.addBan(punishment.name, punishment.reason, Date(punishment.end), punishment.operator)
+                nameBanList().addBan(punishment.name, punishment.reason, Date(punishment.end), punishment.operator)
             }
 
             PunishmentType.IP_BAN, PunishmentType.TEMP_IP_BAN -> {
-                val banList = Bukkit.getBanList(BanList.Type.IP) as BanList<Any>
-                banList.addBan(punishment.name, punishment.reason, Date(punishment.end), punishment.operator)
+                ipBanList().addBan(punishment.name, punishment.reason, Date(punishment.end), punishment.operator)
             }
 
             else -> Unit
         }
     }
 
-    @Suppress("UNCHECKED_CAST")
     @EventHandler
     fun onRevokePunishment(event: RevokePunishmentEvent) {
         val punishment = event.punishment
         when (punishment.type) {
-            PunishmentType.BAN, PunishmentType.TEMP_BAN -> {
-                val banList = Bukkit.getBanList(BanList.Type.NAME) as BanList<Any>
-                banList.pardon(punishment.name)
-            }
-
-            PunishmentType.IP_BAN, PunishmentType.TEMP_IP_BAN -> {
-                val banList = Bukkit.getBanList(BanList.Type.IP) as BanList<Any>
-                banList.pardon(punishment.name)
-            }
-
+            PunishmentType.BAN, PunishmentType.TEMP_BAN -> nameBanList().pardon(punishment.name)
+            PunishmentType.IP_BAN, PunishmentType.TEMP_IP_BAN -> ipBanList().pardon(punishment.name)
             else -> Unit
         }
     }
