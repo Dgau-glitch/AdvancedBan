fdbe6b025d4df93bc5c493dab73471ec004ce863
Dgau-glitch <dgau5@mail.ru>
2026-05-16 13:23:38 +0700

    Migrate tab completion helpers from Java to Kotlin

---
+++ core/src/main/kotlin/me/leoko/advancedban/utils/tabcompletion/CleanTabCompleter.kt
@@ -0,0 +1,26 @@
+package me.leoko.advancedban.utils.tabcompletion
+
+import me.leoko.advancedban.Universal
+import java.util.ArrayList
+
+open class CleanTabCompleter(private val rawTabCompleter: MutableTabCompleter) : MutableTabCompleter {
+    override fun onTabComplete(user: Any, args: Array<String>): ArrayList<String> {
+        val suggestions = rawTabCompleter.onTabComplete(user, args)
+
+        if (suggestions.isNotEmpty() && suggestions[0] == PLAYER_PLACEHOLDER) {
+            suggestions.removeAt(0)
+            for (player in Universal.get().methods.onlinePlayers) {
+                suggestions.add(Universal.get().methods.getName(player))
+            }
+        }
+
+        if (args.isNotEmpty()) {
+            suggestions.removeIf { s -> !s.startsWith(args[args.size - 1]) }
+        }
+        return suggestions
+    }
+
+    companion object {
+        const val PLAYER_PLACEHOLDER: String = "PLAYERS"
+    }
+}
