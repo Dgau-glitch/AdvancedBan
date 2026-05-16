36ee0500a2d95d0a6835a463ef5789a770de8dce
Dgau-glitch <dgau5@mail.ru>
2026-05-16 19:07:48 +0700

    refactor(core): extract warns/notes and advancedban handlers for command kotlin migration

---
+++ core/src/main/kotlin/me/leoko/advancedban/utils/commandspec/ListGroupHandlers.kt
@@ -0,0 +1,67 @@
+package me.leoko.advancedban.utils.commandspec
+
+import me.leoko.advancedban.Universal
+import me.leoko.advancedban.manager.MessageManager
+import me.leoko.advancedban.manager.PunishmentManager
+import me.leoko.advancedban.utils.Command
+import me.leoko.advancedban.utils.CommandUtils.processName
+import me.leoko.advancedban.utils.PunishmentType
+import me.leoko.advancedban.utils.commands.ListProcessor
+import java.util.function.Consumer
+
+/**
+ * Stage 4: extracted handlers for WARNS/NOTES branches from legacy Command.java.
+ */
+object ListGroupHandlers {
+    val warnsHandler: Consumer<Command.CommandInput> = Consumer { input ->
+        if (input.hasNext() && !(input.primary ?: "").matches("[1-9][0-9]*".toRegex())) {
+            if (!Universal.get().hasPerms(input.sender, "ab.warns.other")) {
+                MessageManager.sendMessage(input.sender, "General.NoPerms", true)
+                return@Consumer
+            }
+
+            ListProcessor(
+                { target -> PunishmentManager.get().getPunishments(target ?: return@ListProcessor emptyList(), PunishmentType.WARNING, true) },
+                "Warns", false, true
+            ).accept(input)
+        } else {
+            if (!Universal.get().hasPerms(input.sender, "ab.warns.own")) {
+                MessageManager.sendMessage(input.sender, "General.NoPerms", true)
+                return@Consumer
+            }
+
+            val name = Universal.get().methods.getName(input.sender)
+            val identifier = me.leoko.advancedban.manager.UUIDManager.get().getUUID(name)
+            ListProcessor(
+                { PunishmentManager.get().getPunishments(identifier ?: return@ListProcessor emptyList(), PunishmentType.WARNING, true) },
+                "WarnsOwn", false, false
+            ).accept(input)
+        }
+    }
+
+    val notesHandler: Consumer<Command.CommandInput> = Consumer { input ->
+        if (input.hasNext() && !(input.primary ?: "").matches("[1-9][0-9]*".toRegex())) {
+            if (!Universal.get().hasPerms(input.sender, "ab.notes.other")) {
+                MessageManager.sendMessage(input.sender, "General.NoPerms", true)
+                return@Consumer
+            }
+
+            ListProcessor(
+                { target -> PunishmentManager.get().getPunishments(target ?: return@ListProcessor emptyList(), PunishmentType.NOTE, true) },
+                "Notes", false, true
+            ).accept(input)
+        } else {
+            if (!Universal.get().hasPerms(input.sender, "ab.notes.own")) {
+                MessageManager.sendMessage(input.sender, "General.NoPerms", true)
+                return@Consumer
+            }
+
+            val name = Universal.get().methods.getName(input.sender)
+            val identifier = me.leoko.advancedban.manager.UUIDManager.get().getUUID(name)
+            ListProcessor(
+                { PunishmentManager.get().getPunishments(identifier ?: return@ListProcessor emptyList(), PunishmentType.NOTE, true) },
+                "NotesOwn", false, false
+            ).accept(input)
+        }
+    }
+}
