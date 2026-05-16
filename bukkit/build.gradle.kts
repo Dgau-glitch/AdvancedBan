5b957d56ba791d8ce2d833506c2ca95e255f98bc
Dgau-glitch <dgau5@mail.ru>
2026-05-16 20:06:57 +0700

    build(bukkit): produce shaded plugin jar with expanded plugin version

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,4 +1,9 @@
-plugins { kotlin("jvm") }
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm")
+    id("com.gradleup.shadow") version "8.3.6"
+}
 
 dependencies {
     compileOnly("dev.folia:folia-api:1.21.11-R0.1-SNAPSHOT")
@@ -6,4 +11,19 @@ dependencies {
     implementation("org.bstats:bstats-bukkit:2.2.1")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation(project(":core"))
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("plugin.yml") {
+        expand(mapOf("version" to project.version))
+    }
+}
+
+tasks.withType<ShadowJar>().configureEach {
+    archiveClassifier.set("")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
